50b4b789a7309d4487fae019965d61f818bf84f3
Major Strasser <strasser@T3096.sduhsd.lan>
2017-11-15 16:57:16 -0800

    Finished the project

--- V.java
+++ V.java
@@ -39,4 +39,46 @@ public class exercise {
 
 public class exercise2 {
     public static void main(String[]args){
-	
+	int var =1;
+	double x1 = 1;
+	double y1 = 1;
+	dobule x2 = 5;
+	double y2 = 1;
+	/***************************************
+	 * DETERMINING FUNCTION TO USE
+	 * IF VAR = 1 : CALCULATE DISTANCE
+	 * IF VAR = 2 : CALCULATE SLOPE
+	 * OTHERWISE  : SHOULDN'T HAPPEN. ERROR
+	 **************************************/
+	if(var == 1){ 
+	    System.out.println("CALCULATING DISTANCE");
+	    calcDist(x1, x2, y1, y2);
+	}
+	else if(var == 2){
+	    System.out.println("CALCULATING SLOPE");
+	    calcSlope(x1, x2, y1, y2);
+	}
+	else{
+	    System.out.println("AN ERROR HAS OCCURRED");
+	    System.out.println("PLEASE CONTACT A SERVICE REPRESENTATIVE");
+	}
+    }
+
+    static void calcdist(x1, x2, y1, y2){
+	dx = Math.abs(x1 - x2);
+	dy = Math.abs(y1 - y2);
+
+	out = Math.sqrt(dx^2 + dy^2);
+
+	System.out.println(out);
+    }
+
+    static void calcSlope(x1, x2, y1, y2){
+	dx = Math.abs(x1 - x2);
+	dy = Math.abs(y1 - y2);
+
+	out = dy / dx;
+
+	System.out.println(out);
+    }
+}
