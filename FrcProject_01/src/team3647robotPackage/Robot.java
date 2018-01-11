2065971224f3a3506dd04d16cff8846d2d652f89
Major Strasser <strasser@T3096.sduhsd.lan>
2018-01-10 17:56:08 -0800

    Something

--- FrcProject_01/src/team3647robotPackage/Robot.java
+++ FrcProject_01/src/team3647robotPackage/Robot.java
@@ -9,13 +9,17 @@ public class Robot extends IterativeRobot
 	/* This program is designed to have the robot switch between
 	 * moving forward or backwards at the touch of a button.
 	 */
+	
 	int direction = 1; //determine if the robot is moving forwards or backwards
 	int gradient = 0.4; //Determine what we want max power to be
+	
+	Joysticks JSO;
+	
 	//This function is run whenever the robot starts. This function is used for any initialization of code
 	@Override
 	public void robotInit() 
 	{
-		
+		JSO = new Joysticks();
 	}
 
 	 //This function runs once, right before autonomous period starts. 
@@ -38,10 +42,14 @@ public class Robot extends IterativeRobot
 	@Override
 	public void teleopPeriodic() 
 	{
+		JSO.updateMainController();
+		if(Joysticks.buttonA){
+			direction = direction * -1;
+		}
 		switch(direction) {
 		case 1: motorPower(1);
 		break;
-		case 0: motorPower(-1);
+		case -1: motorPower(-1);
 		break;
 		default: motorPower(0);
 		}
@@ -50,7 +58,7 @@ public class Robot extends IterativeRobot
 	public void motorPower(int power){
 		power = power * gradient;
 		motorObject.leftMotor.set(power);
-		motorObject.leftMotor.set(power);
+		motorObject.rightMotor.set(-power);
 	}
 	
 	//This is the function that is called during the test
