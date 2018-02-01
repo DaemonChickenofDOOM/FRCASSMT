7934bb14a0a8535aed9f1aa0e8b87f4543fb13c1
Raymond yang <wintersoldier@t-3098>
2018-01-31 18:52:11 -0800

    Project 2, Mk. I*

--- II/FrcProject_02/src/team3647robotPackage/encVal.java
+++ II/FrcProject_02/src/team3647robotPackage/encVal.java
@@ -1,4 +1,11 @@
-public class encVal{
+package team3647robotPackage;
+public class encVal
+{
+	public encVal(int i, int j)
+	{
+		leftEncVal = i;
+		rightEncVal = j;
+	}
 	public double leftEncVal;
 	public double rightEncVal;
 }
