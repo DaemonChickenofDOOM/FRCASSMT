60a792edde9077a1a8a9b0d1c277b9bb340276ad
ry2331 <32783340+ry2331@users.noreply.github.com>
2018-02-14 15:35:05 -0800

    FRC PROJECT III Mk 2 MOD 0

---
+++ III/FrcProject_03/src/team3647robotPackage/encVal.java
@@ -0,0 +1,11 @@
+package team3647robotPackage;
+public class encVal
+{
+	public encVal(int i, int j)
+	{
+		leftEncVal = i;
+		rightEncVal = j;
+	}
+	public double leftEncVal;
+	public double rightEncVal;
+}
